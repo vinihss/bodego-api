64fd8b28a5ac7850889328421ecf660ae5f8486d
Vinicius Heleno <vinihss@gmail.com>
2025-08-30 20:04:45 -0300

    update .env

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,15 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/vinihss/bodego-api/config"
 	"github.com/vinihss/bodego-api/internal/infrastructure/database/repositories"
 	http_interfaces_authentication "github.com/vinihss/bodego-api/internal/interfaces/http/authentcation"
 	customeruse "github.com/vinihss/bodego-api/internal/usecases/customer"
-	newsuse "github.com/vinihss/bodego-api/internal/usecases/news"
+	"github.com/vinihss/bodego-api/internal/usecases/user"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	_ "github.com/vinihss/bodego-api/docs"
 	"github.com/vinihss/bodego-api/internal/interfaces/http/customer"
-	"github.com/vinihss/bodego-api/internal/interfaces/http/news"
 	"github.com/vinihss/bodego-api/middlewares"
 )
 
@@ -21,12 +21,12 @@ func SetupRoutes(router *gin.Engine) {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	authController := http_interfaces_authentication.NewAuthenticationController()
 	router.POST("/authenticate", http_interfaces_authentication.NewAuthenticationHandler(authController).Authenticate)
+	db, _ := gorm.Open(postgres.Open("host=postgres user=postgres password=postgres dbname=bodego port=5432 sslmode=disable"), &gorm.Config{})
 
 	authorized := router.Group("/")
 	authorized.Use(middlewares.JWTAuth())
 	{
-
-		custRepo := repositories.NewCustomerRepository(config.DB)
+		custRepo := repositories.NewCustomerRepository(db)
 		createCustomerUC := customeruse.NewCreateCustomerUseCase(custRepo)
 		deleteCustomerUC := customeruse.NewDeleteCustomerUseCase(custRepo)
 		findCustomerUC := customeruse.NewFindCustomerUseCase(custRepo)
@@ -36,17 +36,13 @@ func SetupRoutes(router *gin.Engine) {
 		custHandler := http_interfaces_customer.NewCustomerHandler(custController)
 		RegisterCustomerRoutes(router, custHandler)
 
-		// News routes
-		newsRepo := repositories.NewNewsRepository(config.DB)
-		createNewsUC := newsuse.NewCreateNewsUseCase(newsRepo)
-		deleteNewsUC := newsuse.NewDeleteNewsUseCase(newsRepo)
-		findNewsUC := newsuse.NewFindNewsUseCase(newsRepo)
-		updateNewsUC := newsuse.NewUpdateNewsUseCase(newsRepo)
-
-		newsController := http_interfaces_news.NewNewsController(createNewsUC, deleteNewsUC, findNewsUC, updateNewsUC)
-		newsHandler := http_interfaces_news.NewNewsHandler(newsController)
-		RegisterNewsRoutes(router, newsHandler)
-
+		// User CRUD (apenas sysadmin)
+		userRepo := repositories.NewUserRepository(db)
+		userCreateUC := user.NewCreateUserUseCase(userRepo)
+		userHandler := http_interfaces_user.NewHandler(userCreateUC)
+		userGroup := authorized.Group("")
+		userGroup.Use(middlewares.OnlySysAdmin())
+		http_interfaces_user.RegisterUserRoutes(userGroup, userHandler)
 	}
 
 }
