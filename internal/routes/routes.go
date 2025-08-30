702ac57bf59f0f1bd3dc7124b3b3449251bb683d
Vinicius Heleno <vinihss@gmail.com>
2025-08-30 20:04:45 -0300

    update .env

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/vinihss/bodego-api/internal/infrastructure/database/repositories"
 	http_interfaces_authentication "github.com/vinihss/bodego-api/internal/interfaces/http/authentcation"
 	customeruse "github.com/vinihss/bodego-api/internal/usecases/customer"
+	"github.com/vinihss/bodego-api/internal/usecases/user"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
@@ -20,28 +21,28 @@ func SetupRoutes(router *gin.Engine) {
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	authController := http_interfaces_authentication.NewAuthenticationController()
 	router.POST("/authenticate", http_interfaces_authentication.NewAuthenticationHandler(authController).Authenticate)
-	db, _ := gorm.Open(postgres.Open("host=dpg-d2jbkfbuibrs73defhq0-a user=posgres password=6REY3Hz6IzIODENTegTSiaac7zkAOL5u dbname=bodego port=5432 sslmode=disable"), &gorm.Config{})
+	db, _ := gorm.Open(postgres.Open("host=postgres user=postgres password=postgres dbname=bodego port=5432 sslmode=disable"), &gorm.Config{})
 
-       authorized := router.Group("/")
-       authorized.Use(middlewares.JWTAuth())
-       {
-	       custRepo := repositories.NewCustomerRepository(db)
-	       createCustomerUC := customeruse.NewCreateCustomerUseCase(custRepo)
-	       deleteCustomerUC := customeruse.NewDeleteCustomerUseCase(custRepo)
-	       findCustomerUC := customeruse.NewFindCustomerUseCase(custRepo)
-	       updateCustomerUC := customeruse.NewUpdateCustomerUseCase(custRepo)
+	authorized := router.Group("/")
+	authorized.Use(middlewares.JWTAuth())
+	{
+		custRepo := repositories.NewCustomerRepository(db)
+		createCustomerUC := customeruse.NewCreateCustomerUseCase(custRepo)
+		deleteCustomerUC := customeruse.NewDeleteCustomerUseCase(custRepo)
+		findCustomerUC := customeruse.NewFindCustomerUseCase(custRepo)
+		updateCustomerUC := customeruse.NewUpdateCustomerUseCase(custRepo)
 
-	       custController := http_interfaces_customer.NewCustomerController(createCustomerUC, deleteCustomerUC, findCustomerUC, updateCustomerUC)
-	       custHandler := http_interfaces_customer.NewCustomerHandler(custController)
-	       RegisterCustomerRoutes(router, custHandler)
+		custController := http_interfaces_customer.NewCustomerController(createCustomerUC, deleteCustomerUC, findCustomerUC, updateCustomerUC)
+		custHandler := http_interfaces_customer.NewCustomerHandler(custController)
+		RegisterCustomerRoutes(router, custHandler)
 
-	       // User CRUD (apenas sysadmin)
-	       userRepo := repositories.NewUserRepository(db)
-	       userCreateUC := user.NewCreateUserUseCase(userRepo)
-	       userHandler := http_interfaces_user.NewHandler(userCreateUC)
-	       userGroup := authorized.Group("")
-	       userGroup.Use(middlewares.OnlySysAdmin())
-	       http_interfaces_user.RegisterUserRoutes(userGroup, userHandler)
-       }
+		// User CRUD (apenas sysadmin)
+		userRepo := repositories.NewUserRepository(db)
+		userCreateUC := user.NewCreateUserUseCase(userRepo)
+		userHandler := http_interfaces_user.NewHandler(userCreateUC)
+		userGroup := authorized.Group("")
+		userGroup.Use(middlewares.OnlySysAdmin())
+		http_interfaces_user.RegisterUserRoutes(userGroup, userHandler)
+	}
 
 }
